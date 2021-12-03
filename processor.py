dfb397d505e07ce88a25822496bc74a0abec694f
qweeze <qweeeze@gmail.com>
2021-12-04 00:01:39 +0500

    add persons and objects embeddings

--- processor.py
+++ processor.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
 import pandas as pd
 from kaldi_speech import KaldiProcessor, ffmpeg_source
+from text_embeddings import SlidingEmbedder
+from video.detections import process_file
 
 
 def process_file(mp4_file, args=None):
@@ -19,7 +22,6 @@ def process_file(mp4_file, args=None):
     tokens_df = pd.DataFrame(ok_tokens)
 
     if args.text_embeddings:
-        from text_embeddings import SlidingEmbedder
 
         model = SlidingEmbedder()
         embeddings = model.make_embeddings(tokens_df.start, tokens_df.word)
@@ -27,8 +29,39 @@ def process_file(mp4_file, args=None):
         result_layers['text-embeddings'] = embeddings
 
     if args.find_peoples:
-        from video.detections import process_file
         persons_data = process_file(mp4_file, 'video/persons.db', max_len = args.max_length)
         result_layers['persons'] = persons_data
+
+        objects_df = pd.DataFrame.from_records(
+            [
+                (timestamp, ' '.join(yolo_tags))
+                for timestamp, yolo_tags in persons_data.objects
+                if yolo_tags
+            ],
+            columns=['timestamp', 'yolo_tags'],
+        )
+
+        person_texts_by_ts = defaultdict(list)
+        for person in persons_data.persons:
+            if person.db_item is None:
+                continue
+
+            for timestamp in person.timestamps:
+                tags = f'{person.db_item.full_name} {person.db_item.description}'
+                person_texts_by_ts[timestamp].append(tags)
+
+        persons_df = pd.DataFrame.from_records(
+            [
+                (timestamp, ' '.join(texts))
+                for timestamp, texts in sorted(person_texts_by_ts.items())
+            ],
+            columns=['timestamp', 'person_text'],
+        )
+
+        model = SlidingEmbedder()
+        person_embeddings = model.make_embeddings(objects_df.timestamp, objects_df.yolo_tags)
+        objects_embeddings = model.make_embeddings(persons_df.timestamp, persons_df.person_text)
+        result_layers['person-embeddings'] = person_embeddings
+        result_layers['objects-embeddings'] = objects_embeddings
 
     return result_layers
